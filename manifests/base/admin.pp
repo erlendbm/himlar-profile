ffb7241c4372cace2e657e1c94c13da21260a866
Jan Ivar Beddari <code@beddari.net>
2014-12-02 15:29:25 +0100

    Include base in admin

--- manifests/base/admin.pp
+++ manifests/base/admin.pp
@@ -2,6 +2,8 @@
 #
 class profile::base::admin
 {
+  include profile::base::base
+
   $modes = hiera('googleauthenticator::pam::mode::modes', {})
   if $modes {
     create_resources('googleauthenticator::pam::mode', $modes)
